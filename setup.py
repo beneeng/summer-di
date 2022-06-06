3775f711920de6312270f254b395c91b3de960c8
Benedikt Engeser <summer-di@418tp.net>
2022-06-06 15:17:07 +0200

    add smth

--- setup.py
+++ setup.py
@@ -8,8 +8,5 @@ setup(
     description='Summer dependency injection and scheduling framework',
     author='Benedikt Engeser',
     license='MIT',
-    install_requires=[],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest==4.4.1'],
-    test_suite='tests',
+    install_requires=[]
 )
