6007daa8a944d9eaa1dac534a0d95f2253a78cd1
Benedikt Engeser <summer-di@418tp.net>
2022-06-06 14:40:38 +0200

    fix package selection

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages, setup
 
+
 setup(
     name='summer',
-    packages=find_packages(include=['summer']),
+    packages=find_packages(include=['summer*']),
     version='0.1.0',
     description='Summer dependency injection and scheduling framework',
     author='Benedikt Engeser',
